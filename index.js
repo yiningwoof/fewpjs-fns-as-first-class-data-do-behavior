8547c29f37eb99de97905f240c7ff0415b017b93
Yining Wang <yining.wang@utexas.edu>
2019-12-09 22:44:52 -0600

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(t) {
+  let hr = t.split(':')[0];
+  if (hr < 12) {
+    return "Good Morning";
+  } else if (hr > 12 && hr < 17) {
+    return "Good Afternoon";
+  } else if (hr > 17 && hr < 24) {
+    return "Good Evening";
+  }
+}
+
+function displayMessage(m) {
+  let greeting = document.querySelector("#greeting");
+  greeting.innerText = m;
+  console.log(greeting.innerText);
+}
